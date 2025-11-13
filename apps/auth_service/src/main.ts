6d4f31044bc410c5f72e9ef25b02e3d56ce9f77e
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-11-13 22:27:02 +0530

    feat: set up express server with middleware and update TypeScript configuration

--- apps/auth_service/src/main.ts
+++ apps/auth_service/src/main.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
 
 const app = express();
 
@@ -10,10 +12,15 @@ app.use(
     credentials: true,
   })
 );
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello API from Auth Service" });
 });
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
