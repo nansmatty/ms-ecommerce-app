17f14408b435ae6e4be29e4ce0f45132c8a4ef18
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-11-18 20:17:22 +0530

    feat: implement email sending functionality and OTP restrictions

--- apps/auth_service/src/utils/auth.helper.ts
+++ apps/auth_service/src/utils/auth.helper.ts
@@ -1,6 +1,8 @@
 import crypto from "crypto";
 import { ValidationError } from "@/packages/error-handler";
 import { NextFunction } from "express";
+import redis from "@/packages/libs/redis";
+import { sendEmail } from "./sendMail";
 
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -24,8 +26,23 @@ export const validateRegistrationData = (
   }
 };
 
-export const checkOtpRestrictions = (email: string, next: NextFunction) => {
-  //
+export const checkOtpRestrictions = async (
+  email: string,
+  next: NextFunction
+) => {
+  if (await redis.get(`otp_lock:${email}`)) {
+    return next(
+      new ValidationError(
+        "Account locked due to multiple failed OTP attempts! Try again after 30 mintues."
+      )
+    );
+  }
+
+  if (await redis.get(`otp_spam_lock:${email}`)) {
+    return next(
+      new ValidationError("Too many OTP requests! Try again after 30 minutes.")
+    );
+  }
 };
 
 export const sendOtp = async (
@@ -34,4 +51,7 @@ export const sendOtp = async (
   template: string
 ) => {
   const otp = crypto.randomInt(1000, 9999).toString();
+  await sendEmail(email, "Verify Your Email", template, { name, otp });
+  await redis.set(`otp:${email}`, otp, "EX", 300); // OTP valid for 5 minutes
+  await redis.set(`otp_cooldown:${email}`, "1", "EX", 60); // 1 minute cooldown
 };
